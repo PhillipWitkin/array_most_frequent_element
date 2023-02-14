984deeb7c3b746dd58739ec65b45fd3c906c796b
Phillip Witkin <pwitkin273@gmail.com>
2023-02-14 07:14:55 -0500

    initial commit

---
+++ mostFrequentElement.js
@@ -0,0 +1,35 @@
+/*
+Given an array of strings, 
+find the one that appears most frequently and the number of times
+it appears. 
+If there are more than one element that occur that many times, 
+pick any of them
+
+Return the most repeated string s, 
+and the number of times n it occurs,
+as an array in the form [s, n]
+*/
+
+let mostRepeatedElement = function(arr){
+
+};
+
+/*
+Example 1
+Input: ["the", "dog", "jumped", "over", "the", "moon"]
+Output: ["the", 2]
+
+Explanation: The string "the" appears 2 times in the array, 
+and every other element only appears once
+
+
+Example 2
+Input: ["a", "abc", "ab", "a", "ab", "abcd", "ab"]
+Output: ["ab", 3]
+
+Explanation: The string "ab" appears 3 times in the array, 
+while all other strings either appear once or twice.
+*/
+
+
+
